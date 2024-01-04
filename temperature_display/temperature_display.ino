b3727533f9fca24b73730b9e67fee5dc0b040474
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-01-04 12:47:33 -0500

    Update lcd display before adding temperature sensor

--- temperature_display/temperature_display.ino
+++ temperature_display/temperature_display.ino
@@ -6,14 +6,22 @@ Displays temperature in C or F on LCD Display,
 
 Based on source found here:
 https://docs.arduino.cc/learn/electronics/lcd-displays#serial-to-display-example
-https://docs.arduino.cc/tutorials/iot-bundle/plant-communicator
+https://docs.arduino.cc/tutorials/iot-bundle/plant-communicator|
+
+Layout: 
+https://docs.arduino.cc/tutorials/giga-r1-wifi/cheat-sheet
 
 Wiring:
 https://docs.arduino.cc/static/7d7b6e99f40c7e55f2e9c6175c6db5b5/260cd/LCD_Base_bb_Fritz.png
 https://docs.arduino.cc/static/e6ac5822aa2a0815f64551beb65139e4/b0681/light__temperature_sensor_rp2040_bfbwQp7XjU.png
+https://www.youtube.com/watch?v=wEbGhYjn4QI
 
 */
+#include <LiquidCrystal.h>
+
 const int tempPin = A1;
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -28,30 +36,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int temperature = get_temperature(); /* assign temperature variable to temperature in Celsius */
-  Serial.print("temperature ");
-  Serial.println(temperature);
-  // clear the screen
-  lcd.clear();
-  // read all the available characters
-  while (Serial.available() > 0) {
-    // display each character to the LCD
-    lcd.write(Serial.read());
-  }
+  int temperature = get_temperature();
+  String tempString = String(temperature);
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature: " + tempString);
   delay(2000);
 }
 
 float get_temperature() {
 
-  return 76; // hardcoded until breaboard arrives
-  // int reading = analogRead(tempPin);
-  // float voltage = reading * 3.3;
-  // voltage /= 1024.0;
+  int reading = analogRead(tempPin);
+  float voltage = reading * 3.3;
+  voltage /= 1024.0;
 
-  // /* temperature in Celsius */
-  // float temperatureC = (voltage - 0.5) * 100; /*converting from 10 mv per degree wit 500 mV offset */
-  // /* Convert to Fahrenheit */
-  // float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  /* temperature in Celsius */
+  float temperatureC = (voltage - 0.5) * 100; /*converting from 10 mv per degree wit 500 mV offset */
+  /* Convert to Fahrenheit */
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 
-  //return temperatureF;
+  return temperatureF;
 }
